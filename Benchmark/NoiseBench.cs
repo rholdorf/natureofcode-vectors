c3295bffc3e9723b97d1d3554c0c08ae353f1637
Rui Holdorf <ruih@pbtech.net.br>
2023-05-16 19:26:02 -0300

    add noise benchmark, OpenSimplex2F is faster

---
+++ Benchmark/NoiseBench.cs
@@ -0,0 +1,73 @@
+using BenchmarkDotNet.Attributes;
+using Noise;
+
+namespace Benchmark;
+
+[MemoryDiagnoser]
+public class NoiseBench
+{
+    private readonly Perlin _noise = new ();
+    private readonly OpenSimplex2S _openSimplex2S = new(777);
+    private readonly OpenSimplex2F _openSimplex2F = new(777);
+    private float _offsetX = 0.1f;
+    private float _offsetY = 0.1f;
+    private float _offsetZ = 0.1f;
+
+    [Benchmark]
+    public void OpenSimplex2F2D()
+    {
+        var x = _openSimplex2F.Noise2(_offsetX, _offsetY);
+        _offsetX += 0.01f;
+        _offsetY += 0.01f;
+    }
+    
+    [Benchmark]
+    public void OpenSimplex2F3D()
+    {
+        var x = _openSimplex2F.Noise3_Classic(_offsetX, _offsetY, _offsetZ);
+        _offsetX += 0.01f;
+        _offsetY += 0.01f;
+        _offsetZ += 0.01f;
+    }    
+    
+    [Benchmark]
+    public void OpenSimplex2S2D()
+    {
+        var x = _openSimplex2S.Noise2(_offsetX, _offsetY);
+        _offsetX += 0.01f;
+        _offsetY += 0.01f;
+    }
+    
+    [Benchmark]
+    public void OpenSimplex2S3D()
+    {
+        var x = _openSimplex2S.Noise3_Classic(_offsetX, _offsetY, _offsetZ);
+        _offsetX += 0.01f;
+        _offsetY += 0.01f;
+        _offsetZ += 0.01f;
+    }    
+
+    [Benchmark]
+    public void PerlinNoise1D()
+    {
+        var x = _noise.Noise(_offsetX);
+        _offsetX += 0.01f;
+    }
+    
+    [Benchmark]
+    public void PerlinNoise2D()
+    {
+        var x = _noise.Noise(_offsetX, _offsetY);
+        _offsetX += 0.01f;
+        _offsetY += 0.01f;
+    }
+    
+    [Benchmark]
+    public void PerlinNoise3D()
+    {
+        var x = _noise.Noise(_offsetX, _offsetY, _offsetZ);
+        _offsetX += 0.01f;
+        _offsetY += 0.01f;
+        _offsetZ += 0.01f;
+    }
+}
---
+++ Benchmark/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Benchmark;
+using BenchmarkDotNet.Running;
+
+Console.WriteLine("Hello, World!");
+
+var summ = BenchmarkRunner.Run<NoiseBench>();
